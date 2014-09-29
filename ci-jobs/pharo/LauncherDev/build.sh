49c5e3cbe695530e4869efd44385ce7bcecc8133
Damien Cassou <damien.cassou@gmail.com>
2014-09-29 09:58:28 +0200

    Fix file name

--- ci-jobs/pharo/LauncherDev/build.sh
+++ ci-jobs/pharo/LauncherDev/build.sh
@@ -28,4 +28,4 @@ bash ./pharo-build/build-platform.sh -i One/Pharo -o Pharo -v $VERSION-$DATE -t 
 
 zip -9r PharoLauncher-user-$VERSION-$DATE.zip PharoLauncher.image PharoLauncher.changes
 
-md5sum PharoLauncher-user--$VERSION-$DATE.zip > PharoLauncher-user--$VERSION-$DATE.zip.md5sum
+md5sum PharoLauncher-user-$VERSION-$DATE.zip > PharoLauncher-user-$VERSION-$DATE.zip.md5sum
