155739591eb4f1e7d9787894a07a9b42db33a5da
Damien Cassou <damien.cassou@gmail.com>
2014-10-01 15:36:18 +0200

    Generate md5sum as well

--- ci-jobs/pharo/Launcher-Mac/build.sh
+++ ci-jobs/pharo/Launcher-Mac/build.sh
@@ -11,3 +11,6 @@ cd ..
 unzip "$launcher_directory/Pharo-mac.zip" 
 cp pharo-ci/background/background.png .
 ./pharo-ci/build-dmg.sh
+
+generated_dmg=*.dmg
+md5sum "$generated_dmg" > "$generated_dmg.md5sum"
