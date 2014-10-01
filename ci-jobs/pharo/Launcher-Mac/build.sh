4c31f1e6b22b76442bdf9510d562ce26c3f9bbdc
Damien Cassou <damien.cassou@gmail.com>
2014-10-01 13:09:02 +0200

    Get the zip from the correct path

--- ci-jobs/pharo/Launcher-Mac/build.sh
+++ ci-jobs/pharo/Launcher-Mac/build.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
-unzip PharoLauncher-*-mac.zip
+cd ./*VERSION=[0-9]*
+launcher_directory=$(pwd)
+# launcher_version=$(basename "$(pwd)" \
+#     | sed -e 's/^.*VERSION=\([0-9.]*\),.*$/\1/')
+
+unzip "$launcher_directory/PharoLauncher-*-mac.zip" 
 cp pharo-ci/background/background.png .
 ./pharo-ci/build-dmg.sh
