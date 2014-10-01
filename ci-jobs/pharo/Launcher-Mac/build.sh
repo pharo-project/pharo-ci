4a9c0970ad75e75f93e7e61ce5570871cb66f6de
Damien Cassou <damien.cassou@gmail.com>
2014-10-01 14:54:23 +0200

    Change name of zip file

--- ci-jobs/pharo/Launcher-Mac/build.sh
+++ ci-jobs/pharo/Launcher-Mac/build.sh
@@ -8,6 +8,6 @@ launcher_directory=$(pwd)
 #     | sed -e 's/^.*VERSION=\([0-9.]*\),.*$/\1/')
 cd ..
 
-unzip "$launcher_directory/PharoLauncher-*-mac.zip" 
+unzip "$launcher_directory/Pharo-mac.zip" 
 cp pharo-ci/background/background.png .
 ./pharo-ci/build-dmg.sh
