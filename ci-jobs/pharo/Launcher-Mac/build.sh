cc9f565bffad3a8c9612a8f6c91b3821a8dbb523
Damien Cassou <damien.cassou@gmail.com>
2014-10-01 15:59:30 +0200

    Use upstream version number

--- ci-jobs/pharo/Launcher-Mac/build.sh
+++ ci-jobs/pharo/Launcher-Mac/build.sh
@@ -4,13 +4,14 @@ set -ex
 
 cd ./*VERSION=[0-9]*
 launcher_directory=$(pwd)
-# launcher_version=$(basename "$(pwd)" \
-#     | sed -e 's/^.*VERSION=\([0-9.]*\),.*$/\1/')
+launcher_version=$(basename "$(pwd)" \
+    | sed -e 's/^.*VERSION=\([0-9.]*\),.*$/\1/')
 cd ..
 
 unzip "$launcher_directory/Pharo-mac.zip" 
 cp pharo-ci/background/background.png .
-./pharo-ci/build-dmg.sh
+
+VERSION=$launcher_version ./pharo-ci/build-dmg.sh
 
 generated_dmg=$(echo *.dmg)
 md5 "$generated_dmg" > "$generated_dmg.md5sum"
