9ba5b52edb1b784a8e9f24f4a85ea3263aa8af39
Damien Cassou <damien.cassou@gmail.com>
2014-10-16 18:20:42 +0200

    Remove commented code

--- ci-jobs/pharo/Launcher-Mac/build.sh
+++ ci-jobs/pharo/Launcher-Mac/build.sh
@@ -4,8 +4,6 @@ set -ex
 
 cd ./*VERSION=[0-9]*
 launcher_directory=$(pwd)
-# launcher_version=$(basename "$(pwd)" \
-#     | sed -e 's/^.*VERSION=\([0-9.]*\),.*$/\1/')
 cd ..
 
 unzip "$launcher_directory/PharoLauncher-*-mac.zip" 
