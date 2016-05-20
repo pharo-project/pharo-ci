69c9d725d5a9b9fb19086ee8dda83f7e295b80a6
DEMAREY Christophe <christophe.demarey@inria.fr>
2016-05-20 14:20:39 +0200

    remove duplicated files from https://github.com/pharo-project/pharo-build-scripts

--- background/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Make sure that the png will look good on Mac OS X when set as a
-# folder background
-convert background.png -flatten -alpha Off -density 28.35x28.35 \
-    -units PixelsPerCentimeter background.temp.png
-
-mv background.temp.png background.png
--- build-windows-installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION=${VERSION:-"3.0.0"}
-
-cd pharo-ci/windows-installer
-mkdir Pharo-win
-mv ../../Pharo Pharo-win/
-NSIS/Bin/makensis.exe pharo-installer-builder.nsi
-mv pharo_installer.exe ../../pharo_installer-"$VERSION".exe
