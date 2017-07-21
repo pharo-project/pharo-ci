5ee17766e83e9b14a8cc7138a86d489233900ffa
Christophe Demarey <demarey@193-51-236-195.lille.inria.fr>
2017-07-21 13:54:57 +0200

    remove pre-spur VM and rename linux archive with the version number

--- ci-jobs/pharo/Launcher/build.sh
+++ ci-jobs/pharo/Launcher/build.sh
@@ -9,28 +9,6 @@ function copy_current_stable_image() {
     mv "$IMAGES_PATH/latest.zip" "$IMAGES_PATH/pharo-stable.zip"
 }
 
-function copy_prespur_vm() {
-	TARGET_OS_TYPE=$1
-	VM_PATH=$TARGET_OS_TYPE/vm
-	VM_PRESPUR="pharo-$TARGET_OS_TYPE-stable.zip"
-	VM_PRESPUR_PATH="$VM_PATH/$VM_PRESPUR"
-	test -f $VM_PRESPUR_PATH || wget -P $VM_PATH http://files.pharo.org/get-files/40/$VM_PRESPUR
-
-	if [ -f "$VM_PRESPUR_PATH" ] ; then
-		mkdir -p $VM_PATH/40
-	    unzip -q "$VM_PRESPUR_PATH" -d "$VM_PATH/40"
-	    rm "$VM_PRESPUR_PATH"
-		cd $VM_PATH/40
-		wget --quiet http://file-pharo.inria.fr/sources/PharoV10.sources.zip && unzip PharoV10.sources.zip PharoV10.sources && rm PharoV10.sources.zip
-		wget --quiet http://file-pharo.inria.fr/sources/PharoV20.sources.zip && unzip PharoV20.sources.zip PharoV20.sources && rm PharoV20.sources.zip
-		wget --quiet http://file-pharo.inria.fr/sources/PharoV30.sources.zip && unzip PharoV30.sources.zip PharoV30.sources && rm PharoV30.sources.zip
-		wget --quiet http://file-pharo.inria.fr/sources/PharoV40.sources.zip && unzip PharoV40.sources.zip PharoV40.sources && rm PharoV40.sources.zip
-		cd -
-	else
-	    echo "Warning: Cannot find pre-spur VM for $TARGET_OS_TYPE!"
-	fi
-}
-
 function ensure_pharo_sources_version() {
 	HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://file-pharo.inria.fr/sources/PharoV$1.sources.zip)
 	if [ $HTTP_CODE -eq 404 ]
@@ -62,11 +40,6 @@ cp PharoLauncher.changes One/Pharo.changes
 cd One
 wget --quiet http://file-pharo.inria.fr/sources/PharoV50.sources.zip && unzip PharoV50.sources.zip PharoV50.sources && rm PharoV50.sources.zip
 copy_current_stable_image
-# prepare platform-specific bundles
-for OS_TYPE in "linux" "mac" "win"
-do
-	copy_prespur_vm $OS_TYPE
-done
 cd ..
 
 DATE=$(date +%Y.%m.%d)
@@ -74,6 +47,7 @@ ensure_pharo_sources_version
 bash ./pharo-build-scripts/build-platform.sh -i Pharo -o Pharo -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t Pharo -p mac
 bash ./pharo-build-scripts/build-platform.sh -i Pharo -o Pharo -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t Pharo -p win
 bash ./pharo-build-scripts/build-platform.sh -i Pharo -o Pharo -r $PHARO -s $PHARO_SOURCES -v $VERSION-$DATE -t Pharo -p linux
+mv Pharo-linux.zip Pharo-linux-$VERSION.zip
 
 zip -9r PharoLauncher-user-$VERSION-$DATE.zip PharoLauncher.image PharoLauncher.changes
 
