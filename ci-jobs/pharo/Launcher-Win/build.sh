79887cdd565cae062f38d69629b48659a3fc92f9
Christophe Demarey <demarey@193-51-236-195.lille.inria.fr>
2017-07-21 16:59:15 +0200

    Update windows build script for launcher

--- ci-jobs/pharo/Launcher-Win/build.sh
+++ ci-jobs/pharo/Launcher-Win/build.sh
@@ -2,16 +2,10 @@
 
 set -ex
 
-cd ./*VERSION=[0-9]*
-launcher_directory=$(pwd)
-launcher_version=$(basename "$(pwd)" \
-    | sed -e 's/^.*VERSION=\([0-9.]*\),.*$/\1/')
-cd ..
+find . -wholename '*VERSION=stable*/Pharo-win.zip' -exec unzip '{}' -d . \;
+find . -wholename '*VERSION=stable*/launcher-version.txt' -exec mv '{}' . \;
 
-unzip "$launcher_directory/Pharo-win.zip"
-
-pwd
-VERSION=$launcher_version ./pharo-build-scripts/build-windows-installer.sh
+VERSION=$(cat launcher-version.txt) ./pharo-build-scripts/build-windows-installer.sh
 
 generated_exe=$(echo *.exe)
 cp "$generated_exe" latest.exe
